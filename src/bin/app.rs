eb9a062e6cfc6cd5642df5d7bda548ac04b98a13
Nobuyuki Sakai <gonhainu@users.noreply.github.com>
2025-08-02 18:01:33 +0900

    feat: enhance logging and error handling in the application

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,15 +1,24 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use adapter::database::connect_database_with;
+use anyhow::Context;
 use anyhow::{Error, Result};
 use api::route::{book::build_book_routers, health::build_health_check_routes};
 use axum::Router;
 use registry::AppRegistry;
 use shared::config::AppConfig;
+use shared::env::{Environment, which};
 use tokio::net::TcpListener;
+use tower_http::LatencyUnit;
+use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
+use tracing::Level;
+use tracing_subscriber::EnvFilter;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    init_logger()?;
     bootstrap().await
 }
 
@@ -22,12 +31,51 @@ async fn bootstrap() -> Result<()> {
     let app = Router::new()
         .merge(build_health_check_routes())
         .merge(build_book_routers())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Micros),
+                ),
+        )
         .with_state(registry);
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(&addr).await?;
 
-    println!("Listening on {addr}");
+    tracing::info!("Listening on {addr}");
+    axum::serve(listener, app)
+        .await
+        .contaext("Unexpected error happened in server")
+        .inspect_err(|e| {
+            tracing::error!(
+                error.cause_chain = ?e,
+                error.message = %e,
+                "Unexpected error"
+            )
+        })
+}
 
-    axum::serve(listener, app).await.map_err(Error::from)
+fn init_logger() -> Result<()> {
+    let log_level = match which() {
+        Environment::Development => "debug",
+        Environment::Production => "info",
+    };
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+
+    let subscriber = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false);
+
+    tracing_subscriber::registry()
+        .with(subscriber)
+        .with(env_filter)
+        .try_init()?;
+
+    Ok(())
 }
