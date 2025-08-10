9e1352d3a89d298c899c87d31457477c5fbad4cf
Nobuyuki Sakai <gonhainu@users.noreply.github.com>
2025-08-10 21:43:24 +0900

    feat: add mockall automation to repository traits for testing

--- kernel/src/repository/auth.rs
+++ kernel/src/repository/auth.rs
@@ -6,11 +6,12 @@ use crate::model::{
     id::UserId,
 };
 
+#[mockall::automock]
 #[async_trait]
 pub trait AuthRepository: Send + Sync {
     async fn fetch_user_id_from_token(
         &self,
-        access_toke: &AccessToken,
+        access_token: &AccessToken,
     ) -> AppResult<Option<UserId>>;
     async fn verify_user(&self, email: &str, password: &str) -> AppResult<UserId>;
     async fn create_token(&self, event: CreateToken) -> AppResult<AccessToken>;
